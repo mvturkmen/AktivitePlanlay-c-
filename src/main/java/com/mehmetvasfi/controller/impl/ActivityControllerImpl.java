71c69885961ab2f9a1edd2d035e09235b6d070af
mvturkmen <mehmetvasfi45@gmail.com>
2024-12-12 01:26:14 +0300

    Updated Activity services and controller

--- src/main/java/com/mehmetvasfi/controller/impl/ActivityControllerImpl.java
+++ src/main/java/com/mehmetvasfi/controller/impl/ActivityControllerImpl.java
@@ -3,9 +3,10 @@ package com.mehmetvasfi.controller.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +32,29 @@ public class ActivityControllerImpl implements IActivityController {
 		activityService.saveActivity(activity);
 		return activity;
 	}
+	
+	@Override
+	@GetMapping(path = "/list/{id}")
+	public Activity getActivityById(@PathVariable(name = "id") Integer id) {
+		
+		return activityService.getActivityById(id);
+	}
+	
+	@DeleteMapping(path = "/delete/{id}")
+	@Override
+	public boolean deleteActivity(@PathVariable(name = "id") Integer id) {
+		return activityService.deleteActivity(id);
+		
+	}
+	
+	@PostMapping(path = "/update/{id}")
+	@Override
+	public Activity updateActivity(@PathVariable  Integer id, @RequestBody Activity activity) {
+		
+		return activityService.updateActivity(id, activity);
+	}
+	
+	
 
 	
 }
--- src/main/java/com/mehmetvasfi/service/IActivityService.java
+++ src/main/java/com/mehmetvasfi/service/IActivityService.java
@@ -7,6 +7,12 @@ import com.mehmetvasfi.entites.Activity;
 public interface IActivityService {
 	
 	public List<Activity> getAllActivity();
+	
 	public Activity saveActivity(Activity activity);
+	
+	public Activity getActivityById(Integer id);
+	
+	public boolean deleteActivity(Integer id);
 
+	public Activity updateActivity(Integer id,Activity activity);
 }
